20aaec2afe263c25c0c2d3b1a663b40e13268ffe
pratik <pdhakal906@gmail.com>
2026-07-26 10:33:54 +0545

    revert to intial state

--- scraper.py
+++ scraper.py
@@ -12,21 +12,8 @@ REVALIDATE_SECRET = os.getenv("REVALIDATE_SECRET")
 # ---------- GLOBAL REUSABLE OBJECTS (IMPORTANT FOR LAMBDA WARM START) ----------
 
 session = requests.Session()
-# session.headers.update({"User-Agent": "PostmanRuntime/7.36.0"})
-# session.headers.update(
-#     {
-#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#         "Accept-Language": "en-US,en;q=0.9",
-#         "Referer": "https://www.google.com/",
-#     }
-# )
-session.proxies.update(
-    {
-        "http": "http://145.220.226.144:8080",
-        "https": "http://145.220.226.144:8080",
-    }
-)
+session.headers.update({"User-Agent": "PostmanRuntime/7.36.0"})
+
 session.timeout = 30  # default timeout
 
 connection = None
